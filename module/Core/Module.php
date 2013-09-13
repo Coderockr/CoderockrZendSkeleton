0721ef35d11091fb2dc63df9999ce872cc94b9df
Elton Minetto <eminetto@gmail.com>
2013-09-13 10:36:02 -0300

    Remoção de código que deveria estar no ApiModule

--- module/Core/Module.php
+++ module/Core/Module.php
@@ -19,14 +19,6 @@ class Module
         $moduleManager = $event->getApplication()->getServiceManager()->get('modulemanager');
         /** @var \Zend\EventManager\SharedEventManager $sharedEvents */
         $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
-
-        //evento de erros da aplicação
-        $sharedEvents->attach(
-            'Zend\Mvc\Application',
-            MvcEvent::EVENT_DISPATCH_ERROR,
-            array($this, 'errorProcess'),
-            999
-        );
     }
 
     /**
@@ -78,66 +70,5 @@ class Module
                 }
             ),
         );
-    }
-
-    /**
-     * Faz o processamento dos erros da aplicação
-     * @param MvcEvent $event
-     * @return null|\Zend\Http\PhpEnvironment\Response
-     */
-    public function errorProcess(MvcEvent $event)
-    {
-        $routeMatch = $event->getRouteMatch();
-
-        $formatter = $routeMatch->getParam('formatter', false);
-        if ($formatter == false) {
-            return;
-        }
-        /** @var \Zend\Di\Di $dInjector */
-        $dInjector = $event->getApplication()->getServiceManager()->get('di');
-
-        $eventParams = $event->getParams();
-
-        /** @var array $configuration */
-        $configuration = $event->getApplication()->getConfig();
-
-        $vars = array();
-        if (isset($eventParams['exception'])) {
-            /** @var \Exception $exception */
-            $exception = $eventParams['exception'];
-
-            if ($configuration['errors']['show_exceptions']['message']) {
-                $vars['error-code'] = $exception->getCode();
-                $vars['error-message'] = $exception->getMessage();
-            }
-            
-            if ($configuration['errors']['show_exceptions']['trace']) {
-                $vars['error-trace'] = $exception->getTrace();
-            }
-        }
-
-        if (empty($vars)) {
-            $vars['error'] = 'Something went wrong';
-        }
-
-        /** @var PostProcessor\AbstractPostProcessor $postProcessor */
-        $postProcessor = $dInjector->get(
-            $configuration['errors']['post_processor'],
-            array('vars' => $vars, 'response' => $event->getResponse())
-        );
-                
-        $postProcessor->process();
-
-        if ($eventParams['error'] === \Zend\Mvc\Application::ERROR_CONTROLLER_NOT_FOUND ||
-            $eventParams['error'] === \Zend\Mvc\Application::ERROR_ROUTER_NO_MATCH
-        ) {
-            $event->getResponse()->setStatusCode(\Zend\Http\PhpEnvironment\Response::STATUS_CODE_501);
-        } else {
-            $event->getResponse()->setStatusCode(\Zend\Http\PhpEnvironment\Response::STATUS_CODE_500);
-        }
-
-        $event->stopPropagation();
-
-        return $postProcessor->getResponse();
     }
 }
